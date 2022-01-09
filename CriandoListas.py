00d35c14be04b73f9acffa52862826e5de44701a
Diego Ferreira <81916034+diego-dev-web@users.noreply.github.com>
2022-01-09 05:34:07 -0300

    Capitulo3-Listas

---
+++ CriandoListas.py
@@ -0,0 +1,8 @@
+#lista preencida estaticamente
+lista_estatica = ["xpto", True]
+
+#lista preenchida dinamicamente
+lista_dinamica = [input("Digite o nome do usuário :"), bool(int(input("EStá logado?")))]
+
+#lista vazia
+lista_vazia = []
